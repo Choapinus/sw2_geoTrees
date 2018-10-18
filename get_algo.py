76c198b57968478f51195dab895709ff884dac47
Felipe <f.muoz328@gmail.com>
2018-10-18 17:13:44 -0300

    Se agrega consumo de API y .env

--- get_algo.py
+++ get_algo.py
@@ -3,28 +3,61 @@ import requests
 import urllib
 from urllib.parse import quote, urlencode
 from pprint import pprint
+from dotenv import load_dotenv
+from os import getenv
 
-api_token = 'AIzaSyA-HXVa2jtkGfKtIJwisxgC46RaWqC1xuI'
-api_url_base = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
-lugar = input()
-lugar = {'address': lugar}
+load_dotenv()
 
-address = urlencode(lugar)
+#api_token = 'AIzaSyA-HXVa2jtkGfKtIJwisxgC46RaWqC1xuI'
 
-key = {'key': api_token}
-key = urlencode(key)
 
-api_url_base += address + '&' + key
+def get_location(lugar=None):
+	api_token = getenv("API_KEY")
+	api_url_base = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-response = requests.get(api_url_base)
+	if lugar:
+		lugar = {'address': lugar}
+		address = urlencode(lugar)
+		key = {'key': api_token}
+		key = urlencode(key)
+		api_url_base += address + '&' + key
+		response = requests.get(api_url_base)
 
-if response:
-	#print(response.status_code)
-	json_response = json.loads(response.content.decode('utf-8'))
-	pprint(json_response.items())
-	#for key,value in json_response.items():
-	#	print("key: {key} | value: {value}".format(key=key, value=value))
-else:
-	print("Ups! Algo salio mal!!")
+		if response:
+			json_response = json.loads(response.content.decode('utf-8'))
+			latitud = json_response["results"][0]["geometry"]["location"]["lat"]
+			longitud = json_response["results"][0]["geometry"]["location"]["lng"]
+			data = {
+				"address": json_response["results"][0]["formatted_address"],
+				"location": {
+					"lat": latitud,
+					"lng": longitud,
+				}
+			}
+			return json.dumps(data)
+		else:
+			data = {
+				"ok": False,
+				"status": 404,
+				"error":{
+					"message": 'Error',
+				}
+			}
+			return json.dumps(data)
 
+	else:
+		data = {
+			"ok": False,
+			"status": 204,
+			"error":{
+				"message": 'Address not specified',
+			}
+		}
+		return json.dumps(data)
+
+#print(api_token)
+#lugar = input()
+#lugar = "antonio varas, providencia"
+
+get_location(lugar)
