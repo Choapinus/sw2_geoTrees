2eced32733940b3a6d017448ee56a9418655f1a3
gustavogalvez <gustavogalvezamail.com>
2018-10-21 14:33:03 -0300

    a

--- appmovil/src/pages/register/register.module.ts
+++ appmovil/src/pages/register/register.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-<<<<<<< HEAD
 import { register } from './register';
 
 @NgModule({
@@ -12,16 +11,3 @@ import { register } from './register';
   ],
 })
 export class RegisterModule {}
-=======
-import { RegisterPage } from './register';
-
-@NgModule({
-  declarations: [
-    RegisterPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(RegisterPage),
-  ],
-})
-export class RegisterPageModule {}
->>>>>>> 4c1217bc5eac761c6f69b56ab14bd73dd808b40a
