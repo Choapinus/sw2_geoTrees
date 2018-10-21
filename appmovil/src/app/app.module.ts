8fd1461fc3736b769e224bd301ec4989aef4c515
gustavogalvez <gustavogalvezamail.com>
2018-10-21 13:43:10 -0300

    actualizacion VC

---
+++ appmovil/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { RegisterPage } from '../pages/register/register';
+
+//import { Camera } from '@ionic-native/camera';
+
+
+
+/*class CameraMock extends Camera {
+  getPicture(options) {
+    return new Promise((resolve, reject) => {
+      resolve("BASE_64_ENCODED_DATA_GOES_HERE");
+    })
+  }
+}*/
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    RegisterPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    RegisterPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+/*{ provide: Camera, useClass: CameraMock }*/
+  ]
+})
+export class AppModule {}
