190b7b8b3b515ca0feafbc492de4be6cc0806d8e
KenBreaker <xkosher00@gmail.com>
2018-12-03 02:04:35 -0300

    Fix de url

--- front-end/src/app/components/map/map.component.ts
+++ front-end/src/app/components/map/map.component.ts
@@ -4,54 +4,54 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+	selector: 'app-map',
+	templateUrl: './map.component.html',
+	styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  title = 'Mapa';
-  trees: any;
-  info: any;
-  response = 0;
-  lat_init: number;
-  lng_init: number;
-  profile;
-  user = {
-    email: '',
-    name: ''
-  };
-  constructor(private _treesService: TreesService,
-              private router: Router, private auth: AuthService) {
-                this._treesService.getTrees().subscribe(
-                  data => {
-                    this.trees = data.data;
-                  }
-                );
-                this.lat_init = -33.434986;
-                this.lng_init = -70.614955;
-                auth.handleAuthentication();
-                if (this.auth.userProfile) {
-                  this.profile = this.auth.userProfile;
-                } else {
-                  this.auth.getProfile((err, profile) => {
-                  });
-                }
-  }
-  ngOnInit() {
-    if (this.auth.userProfile) {
-      this.profile = this.auth.userProfile;
-    } else {
-      this.auth.getProfile((err, profile) => {
-      });
-    }
-  }
-  sendReport( idx: number ) {
-    this.router.navigate( ['/sendreport', idx] );
-  }
-  seeMore( idx: number ) {
-    this.router.navigate( ['/seemore', idx] );
-  }
-  getData( ) {
-    return this.trees;
-  }
+	title = 'Mapa';
+	trees: any;
+	info: any;
+	response = 0;
+	lat_init: number;
+	lng_init: number;
+	profile;
+	user = {
+		email: '',
+		name: ''
+	};
+	constructor(private _treesService: TreesService,
+		private router: Router, private auth: AuthService) {
+		this._treesService.getTrees().subscribe(
+			data => {
+				this.trees = data.data;
+			}
+		);
+		this.lat_init = -33.434986;
+		this.lng_init = -70.614955;
+		auth.handleAuthentication();
+		if (this.auth.userProfile) {
+			this.profile = this.auth.userProfile;
+		} else {
+			this.auth.getProfile((err, profile) => {
+			});
+		}
+	}
+	ngOnInit() {
+		if (this.auth.userProfile) {
+			this.profile = this.auth.userProfile;
+		} else {
+			this.auth.getProfile((err, profile) => {
+			});
+		}
+	}
+	sendReport(idx: number) {
+		this.router.navigate(['/sendreport', idx]);
+	}
+	seeMore(idx: number) {
+		this.router.navigate(['/seemore', idx]);
+	}
+	getData() {
+		return this.trees;
+	}
 }
--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -8,68 +8,68 @@ import * as auth0 from 'auth0-js';
 
 @Injectable()
 export class AuthService {
-  public userProfile;
-  auth0 = new auth0.WebAuth({
-    clientID: 'bCKbtLxdU0gM8qQ3dalmho4O7gJd051N',
-    domain: 'comunitree.auth0.com',
-    responseType: 'token id_token',
-    redirectUri: 'http://localhost:80/home',
-    scope: 'openid profile'
-  });
+	public userProfile;
+	auth0 = new auth0.WebAuth({
+		clientID: 'bCKbtLxdU0gM8qQ3dalmho4O7gJd051N',
+		domain: 'comunitree.auth0.com',
+		responseType: 'token id_token',
+		redirectUri: 'http://comunitree.tk:80/home',
+		scope: 'openid profile'
+	});
 
-  constructor(public router: Router) {}
+	constructor(public router: Router) { }
 
-  public login(): void {
-    this.auth0.authorize();
-  }
-  public handleAuthentication(): void {
-    this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        this.setSession(authResult);
-        this.router.navigate(['/home']);
-      } else if (err) {
-        this.router.navigate(['/home']);
-        console.log(err);
-      }
-    });
-  }
+	public login(): void {
+		this.auth0.authorize();
+	}
+	public handleAuthentication(): void {
+		this.auth0.parseHash((err, authResult) => {
+			if (authResult && authResult.accessToken && authResult.idToken) {
+				window.location.hash = '';
+				this.setSession(authResult);
+				this.router.navigate(['/home']);
+			} else if (err) {
+				this.router.navigate(['/home']);
+				console.log(err);
+			}
+		});
+	}
 
-  private setSession(authResult): void {
-    // Set the time that the Access Token will expire at
-    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
-  }
+	private setSession(authResult): void {
+		// Set the time that the Access Token will expire at
+		const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+		localStorage.setItem('access_token', authResult.accessToken);
+		localStorage.setItem('id_token', authResult.idToken);
+		localStorage.setItem('expires_at', expiresAt);
+	}
 
-  public logout(): void {
-    // Remove tokens and expiry time from localStorage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    // Go back to the home route
-    this.router.navigate(['/']);
-  }
+	public logout(): void {
+		// Remove tokens and expiry time from localStorage
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('id_token');
+		localStorage.removeItem('expires_at');
+		// Go back to the home route
+		this.router.navigate(['/']);
+	}
 
-  public isAuthenticated(): boolean {
-    // Check whether the current time is past the
-    // Access Token's expiry time
-    const expiresAt = JSON.parse(localStorage.getItem('expires_at') || '{}');
-    return new Date().getTime() < expiresAt;
-  }
+	public isAuthenticated(): boolean {
+		// Check whether the current time is past the
+		// Access Token's expiry time
+		const expiresAt = JSON.parse(localStorage.getItem('expires_at') || '{}');
+		return new Date().getTime() < expiresAt;
+	}
 
-  public getProfile(cb): void {
-    const accessToken = localStorage.getItem('access_token');
-    if (!accessToken) {
-      throw new Error('Access Token must exist to fetch profile');
-    }
-    const self = this;
-    this.auth0.client.userInfo(accessToken, (err, profile) => {
-      if (profile) {
-        self.userProfile = profile;
-      }
-      cb(err, profile);
-    });
-  }
+	public getProfile(cb): void {
+		const accessToken = localStorage.getItem('access_token');
+		if (!accessToken) {
+			throw new Error('Access Token must exist to fetch profile');
+		}
+		const self = this;
+		this.auth0.client.userInfo(accessToken, (err, profile) => {
+			if (profile) {
+				self.userProfile = profile;
+			}
+			cb(err, profile);
+		});
+	}
 }
